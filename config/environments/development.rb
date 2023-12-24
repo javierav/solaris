d96d3f16061349bdc8bd22e12eb6a31502bd2e67
Javier Aranda <aranda@hey.com>
2023-12-24 15:23:44 +0100

    Add SolidQueue for jobs processing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  ## Active Job
+  config.active_job.queue_adapter = :async
+  config.active_job.verbose_enqueue_logs = true
 end
