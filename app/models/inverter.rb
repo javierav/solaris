36331ad460fed52094c32e398b5f1faaa0bf69a2
Javier Aranda <aranda@hey.com>
2024-01-07 23:40:40 +0100

    Change Inverter model attributes

--- app/models/inverter.rb
+++ app/models/inverter.rb
@@ -1,8 +1,11 @@
 class Inverter < ApplicationRecord
   include Sqideable
 
+  enum :installation_type, %i[domestic other].to_enum_hash
+
   belongs_to :protocol
+  belongs_to :zone
 
   validates :name, presence: true
-  validates :timezone, presence: true
+  validates :installation_type, presence: true
 end
